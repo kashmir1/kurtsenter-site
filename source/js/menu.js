8c16e0373650e27204f39e947bfce65dd60be05d
kashmir1 <53399927+kashmir1@users.noreply.github.com>
2020-11-23 15:31:13 +1000

    поиск

--- source/js/menu.js
+++ source/js/menu.js
@@ -44,3 +44,15 @@ for (var q = 0; q < submenuItems.length; q++) {
   var subList = subLists[q];
   addSubmenuClickListener(submenuItem, subList)
 }
+
+
+// Open the full screen search box
+function openSearch(evt) {
+  evt.preventDefault();
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+// Close the full screen search box
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
